8d130ec315457243c32dba098c4961a52f0c9f49
GuenMo Kim <hybrid0815@gmail.com>
2016-11-02 22:13:04 +0900

    [mod] maya2017

--- renameTool/renameTool/view/mainUI.py
+++ renameTool/renameTool/view/mainUI.py
@@ -1,25 +1,31 @@
 # coding:utf-8
 
-from PySide import QtGui, QtCore
+try:
+    from PySide.QtGui import *
+    from PySide.QtCore import *
+except:
+    from PySide2.QtGui import *
+    from PySide2.QtCore import *
+    from PySide2.QtWidgets import *
 
 import control.namesControl as namesControl
 reload(namesControl)
 
-class RenameUI(QtGui.QDialog):
+class RenameUI(QDialog):
     def __init__(self, parent=None):
         super(RenameUI, self).__init__(parent)
         
         # Window
         self.setWindowTitle('Rename Tool')
-        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
         self.setFixedWidth(320) 
         self.setFixedHeight(280)
         
         # Layout
-        mainLayout = QtGui.QVBoxLayout()
+        mainLayout = QVBoxLayout()
         mainLayout.setContentsMargins(5,5,5,5)
         mainLayout.setSpacing(0)
-        mainLayout.setAlignment(QtCore.Qt.AlignTop)
+        mainLayout.setAlignment(Qt.AlignTop)
         
         renameWidget  = RenameWidget(self)
         replaceWidget = ReplaceWidget(self) 
@@ -29,13 +35,13 @@ class RenameUI(QtGui.QDialog):
         
         self.setLayout(mainLayout)
 
-class RenameWidget(QtGui.QWidget):
+class RenameWidget(QWidget):
     def __init__(self, parent=None):
         super(RenameWidget, self).__init__(parent)
-        mainLayout = QtGui.QVBoxLayout() 
+        mainLayout = QVBoxLayout() 
         mainLayout.setContentsMargins(5,5,5,5)
         mainLayout.setSpacing(2)
-        self.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
         self.setLayout(mainLayout)
         
         renameSplitter = Splitter('RENAME')
@@ -44,28 +50,28 @@ class RenameWidget(QtGui.QWidget):
         splitter3 = Splitter(shadow=False, color=(60,60,60))
         
         # nameLayout
-        nameLayout = QtGui.QHBoxLayout()
+        nameLayout = QHBoxLayout()
         nameLayout.setContentsMargins(4,0,4,0)
         nameLayout.setSpacing(2)
         
-        nameLabel = QtGui.QLabel('New Name:')
+        nameLabel = QLabel('New Name:')
         nameLabel.setFixedWidth(55)
-        self.nameLine  = QtGui.QLineEdit()
-        regEx     = QtCore.QRegExp('^(?!^_)[a-zA-Z_]+')
-        #regEx     = QtCore.QRegExp('^[a-zA-Z][a-zA-Z_]+')
-        nameValidator = QtGui.QRegExpValidator(regEx, self.nameLine)
+        self.nameLine  = QLineEdit()
+        regEx     = QRegExp('^(?!^_)[a-zA-Z_]+')
+        #regEx     = QRegExp('^[a-zA-Z][a-zA-Z_]+')
+        nameValidator = QRegExpValidator(regEx, self.nameLine)
         self.nameLine.setValidator(nameValidator)
         
         nameLayout.addWidget(nameLabel)
         nameLayout.addWidget(self.nameLine)
         
         # multLayout
-        multLayout = QtGui.QHBoxLayout()
+        multLayout = QHBoxLayout()
         multLayout.setContentsMargins(4,0,4,0)
         multLayout.setSpacing(2)
         
-        multLabel = QtGui.QLabel('Multiples Naming Method:')
-        self.multCombo = QtGui.QComboBox()
+        multLabel = QLabel('Multiples Naming Method:')
+        self.multCombo = QComboBox()
         self.multCombo.addItem('Numbers (0-9)')
         self.multCombo.addItem('Letters (a-z)')
         self.multCombo.setFixedWidth(110)
@@ -74,63 +80,63 @@ class RenameWidget(QtGui.QWidget):
         multLayout.addWidget(self.multCombo)
         
         # multOptionLayout
-        multOptionLayout = QtGui.QHBoxLayout()
+        multOptionLayout = QHBoxLayout()
         multOptionLayout.setContentsMargins(4,0,4,0)
         multOptionLayout.setSpacing(2)
         
-        self.paddingLabel = QtGui.QLabel('No. Padding:')
-        self.paddingSpin  = QtGui.QSpinBox()
+        self.paddingLabel = QLabel('No. Padding:')
+        self.paddingSpin  = QSpinBox()
         self.paddingSpin.setFixedWidth(40)
         self.paddingSpin.setMinimum(0)
         self.paddingSpin.setMaximum(10)
         
-        self.lowerRadio = QtGui.QRadioButton('Lowercase')
+        self.lowerRadio = QRadioButton('Lowercase')
         self.lowerRadio.setVisible(False)
         self.lowerRadio.setFixedHeight(23)
         self.lowerRadio.setChecked(True)
-        self.upperRadio = QtGui.QRadioButton('Uppercase')
+        self.upperRadio = QRadioButton('Uppercase')
         self.upperRadio.setVisible(False)
         self.upperRadio.setFixedHeight(23)
         
         multOptionLayout.addWidget(self.paddingLabel)
         multOptionLayout.addWidget(self.paddingSpin)
-        #multOptionLayout.addSpacerItem(QtGui.QSpacerItem(5,5,QtGui.QSizePolicy.Expanding))
+        #multOptionLayout.addSpacerItem(QSpacerItem(5,5,QSizePolicy.Expanding))
         multOptionLayout.addWidget(self.lowerRadio)
         multOptionLayout.addWidget(self.upperRadio)
         
         # multOptionLayout
-        fixLayout = QtGui.QHBoxLayout()
+        fixLayout = QHBoxLayout()
         fixLayout.setContentsMargins(4,0,4,0)
         fixLayout.setSpacing(2)
          
-        self.prefixCheck = QtGui.QCheckBox('Prefix:')
-        self.prefixLine  = QtGui.QLineEdit()
+        self.prefixCheck = QCheckBox('Prefix:')
+        self.prefixLine  = QLineEdit()
         self.prefixLine.setEnabled(False)
         self.prefixLine.setFixedWidth(85)
-        prefixValidator = QtGui.QRegExpValidator(regEx, self.prefixLine)
+        prefixValidator = QRegExpValidator(regEx, self.prefixLine)
         self.prefixLine.setValidator(prefixValidator)
     
-        self.suffixCheck = QtGui.QCheckBox('Suffix:')
-        self.suffixLine  = QtGui.QLineEdit()
+        self.suffixCheck = QCheckBox('Suffix:')
+        self.suffixLine  = QLineEdit()
         self.suffixLine.setEnabled(False)
         self.suffixLine.setFixedWidth(85)
-        suffixRegEx    = QtCore.QRegExp('[0-9a-zA-Z_]+^(?!^_)')
-        suffiValidator = QtGui.QRegExpValidator(suffixRegEx, self.suffixLine)
+        suffixRegEx    = QRegExp('[0-9a-zA-Z_]+^(?!^_)')
+        suffiValidator = QRegExpValidator(suffixRegEx, self.suffixLine)
         self.suffixLine.setValidator(suffiValidator)
         
         fixLayout.addWidget(self.prefixCheck)
         fixLayout.addWidget(self.prefixLine)
-        fixLayout.addSpacerItem(QtGui.QSpacerItem(5,5,QtGui.QSizePolicy.Expanding))
+        fixLayout.addSpacerItem(QSpacerItem(5,5,QSizePolicy.Expanding))
         fixLayout.addWidget(self.suffixCheck)
         fixLayout.addWidget(self.suffixLine)
         
         # bttnLayout
-        bttnLayout = QtGui.QHBoxLayout()
+        bttnLayout = QHBoxLayout()
         bttnLayout.setContentsMargins(4,0,4,0)
         bttnLayout.setSpacing(2)
         
-        self.previewLabel = QtGui.QLabel('e.g.')
-        self.renameBttn  = QtGui.QPushButton('Rename')
+        self.previewLabel = QLabel('e.g.')
+        self.renameBttn  = QPushButton('Rename')
         self.renameBttn.setFixedSize(55, 20)
         
         bttnLayout.addWidget(self.previewLabel)
@@ -218,13 +224,13 @@ class RenameWidget(QtGui.QWidget):
     def rename(self):   
         namesControl.rename(*self._getRenameSetting())
         
-class ReplaceWidget(QtGui.QWidget):
+class ReplaceWidget(QWidget):
     def __init__(self, parent=None):
         super(ReplaceWidget, self).__init__(parent)
-        mainLayout = QtGui.QVBoxLayout() 
+        mainLayout = QVBoxLayout() 
         mainLayout.setContentsMargins(5,5,5,5)
         mainLayout.setSpacing(2)
-        self.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
         self.setLayout(mainLayout)
         
         replaceSplitter = Splitter('FIND/REPLACE')
@@ -232,55 +238,55 @@ class ReplaceWidget(QtGui.QWidget):
         splitter2 = Splitter(shadow=False, color=(60,60,60))
         
         # findLayout
-        findLayout = QtGui.QHBoxLayout()
+        findLayout = QHBoxLayout()
         findLayout.setContentsMargins(4,0,4,0)
         findLayout.setSpacing(2)
         
-        findLabel = QtGui.QLabel('Find:')
+        findLabel = QLabel('Find:')
         findLabel.setFixedWidth(55)
-        self.findLine  = QtGui.QLineEdit()
+        self.findLine  = QLineEdit()
         
         findLayout.addWidget(findLabel)
         findLayout.addWidget(self.findLine)
         
         # replaceLayout
-        replaceLayout = QtGui.QHBoxLayout()
+        replaceLayout = QHBoxLayout()
         replaceLayout.setContentsMargins(4,0,4,0)
         replaceLayout.setSpacing(2)
         
-        replaceLabel = QtGui.QLabel('Replace:')
+        replaceLabel = QLabel('Replace:')
         replaceLabel.setFixedWidth(55)
-        self.replaceLine  = QtGui.QLineEdit()
+        self.replaceLine  = QLineEdit()
         
         replaceLayout.addWidget(replaceLabel)
         replaceLayout.addWidget(self.replaceLine)
         
         # selectionLayout
-        selectionLayout = QtGui.QHBoxLayout()
+        selectionLayout = QHBoxLayout()
         selectionLayout.setContentsMargins(4,0,4,0)
         selectionLayout.setSpacing(2)
         
-        selectionLabel = QtGui.QLabel('Selection Mode:')
-        self.allRadio       = QtGui.QRadioButton('All')
+        selectionLabel = QLabel('Selection Mode:')
+        self.allRadio       = QRadioButton('All')
         self.allRadio.setFixedHeight(19)
         self.allRadio.setChecked(True)
-        self.selectdRadio   = QtGui.QRadioButton('Selected')
+        self.selectdRadio   = QRadioButton('Selected')
         self.selectdRadio.setFixedHeight(19)
         
         selectionLayout.addWidget(selectionLabel)
-        selectionLayout.addSpacerItem(QtGui.QSpacerItem(5,5,QtGui.QSizePolicy.Expanding))
+        selectionLayout.addSpacerItem(QSpacerItem(5,5,QSizePolicy.Expanding))
         selectionLayout.addWidget(self.allRadio)
         selectionLayout.addWidget(self.selectdRadio)
         
         # replaceBttnLayout
-        replaceBttnLayout = QtGui.QHBoxLayout()
+        replaceBttnLayout = QHBoxLayout()
         replaceBttnLayout.setContentsMargins(4,0,4,0)
         replaceBttnLayout.setSpacing(2)
         
-        replaceBttn = QtGui.QPushButton('Repalce')
+        replaceBttn = QPushButton('Repalce')
         replaceBttn.setFixedSize(55, 20)
         
-        replaceBttnLayout.addSpacerItem(QtGui.QSpacerItem(5,5,QtGui.QSizePolicy.Expanding))
+        replaceBttnLayout.addSpacerItem(QSpacerItem(5,5,QSizePolicy.Expanding))
         replaceBttnLayout.addWidget(replaceBttn)
         
         mainLayout.addWidget(replaceSplitter)
@@ -303,20 +309,20 @@ class ReplaceWidget(QtGui.QWidget):
             flag = 'all'
         namesControl.findReplace(findStr, replaceStr, flag)
         
-class Splitter(QtGui.QWidget):
+class Splitter(QWidget):
     def __init__(self, text=None, shadow=True, color=(150,150,150), parent=None):
         super(Splitter, self).__init__(parent)
         
         self.setMinimumHeight(2)
-        mainLayout = QtGui.QHBoxLayout()
+        mainLayout = QHBoxLayout()
         mainLayout.setContentsMargins(0,0,0,0)
         mainLayout.setSpacing(0)
-        mainLayout.setAlignment(QtCore.Qt.AlignVCenter)
+        mainLayout.setAlignment(Qt.AlignVCenter)
         self.setLayout(mainLayout)
         
         # firstLine
-        firstLine = QtGui.QFrame()
-        firstLine.setFrameStyle(QtGui.QFrame.HLine)
+        firstLine = QFrame()
+        firstLine.setFrameStyle(QFrame.HLine)
         
         # Line Style     
         mainColor     = 'rgba({},{},{},255)'.format(color[0],color[1],color[2])
@@ -339,17 +345,17 @@ class Splitter(QtGui.QWidget):
             return 
         firstLine.setMaximumWidth(5)
         
-        font     = QtGui.QFont()
+        font     = QFont()
         font.setBold(True)
-        textWith = QtGui.QFontMetrics(font)
+        textWith = QFontMetrics(font)
         width    = textWith.width(text) + 6
-        label    = QtGui.QLabel(text)
+        label    = QLabel(text)
         label.setFont(font)
         label.setMaximumWidth(width)
-        label.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+        label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         # secondLine
-        secondLine = QtGui.QFrame()
-        secondLine.setFrameStyle(QtGui.QFrame.HLine)
+        secondLine = QFrame()
+        secondLine.setFrameStyle(QFrame.HLine)
         
         # mainLayout
        
