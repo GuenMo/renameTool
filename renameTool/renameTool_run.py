fb1109ef179d4dac5e75e42954c69da76ec7f057
GuenMo Kim <hybrid0815@gmail.com>
2016-11-02 23:54:02 +0900

    aaa

--- renameTool/renameTool_run.py
+++ renameTool/renameTool_run.py
@@ -1,26 +1,19 @@
 # coding:utf-8
 
-import sys
+def run():
+	try:
+		filePath = __file__
+		appPath = filePath.rpartition("\\")[0]
+	except:
+		print "Application`s path not exist."
 
-def renameTool_run():
-    try:
-        filePath = __file__
-        appPath = filePath.rpartition('\\')[0]
-    except:
-        print 'Environ Value {} not exist.'.format(appPath)
-    
-    else:
-        path = appPath
-        
-        if not path in sys.path:
-            sys.path.append(path)
-        
-        import view.mainUI as RenameToolUI
-        reload(RenameToolUI)
-        RenameToolUI.main()
-        
-if __name__ == 'renameTool_run':  
-    renameTool_run()
+	else:
+		import sys
+		path = appPath
 
+		if not path in sys.path:
+			sys.path.append(path)
 
-
+		import view.mainUI as RenameToolUI
+		reload(RenameToolUI)
+		RenameToolUI.main()
